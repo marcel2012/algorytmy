bf57b8d40f48d3e3f79ddcbdb39c26a10dcbef3b
Marcel <marcel2012@users.noreply.github.com>
2016-12-20 12:33:49 +0100

    Update fraktale.cpp

--- fraktale/fraktale.cpp
+++ fraktale/fraktale.cpp
@@ -1,8 +1,7 @@
 #include <SFML/Graphics.hpp>
-using namespace std;
-int wx=800,wy=800;
-int k_height=50;
-int d_height=180;
+#include <windows.h>
+#include <cmath>
+int w=500;
 void linia(int x1,int y1,int x2,int y2,sf::RenderWindow &window)
 {
     sf::Vertex point[2];
@@ -10,71 +9,173 @@ void linia(int x1,int y1,int x2,int y2,sf::RenderWindow &window)
     point[1].position=sf::Vector2f(x2,y2);
     window.draw(point,2, sf::Lines);
 }
-void kantor(int x1,int x2,int y,sf::RenderWindow &window)
+int po(int a,int b)
 {
-    if(y<wy)
+    int odp=1;
+    while(b>=1)
     {
-        linia(x1,y,x2,y,window);
-        kantor(x1,x1+(x2-x1)/3,y+k_height,window);
-        kantor(x1+(x2-x1)*2/3,x2,y+k_height,window);
+        odp*=a;
+        b--;
+    }
+    return odp;
+}
+void kwadraty(int x,int y,int poziom,sf::RenderWindow &window)
+{
+    if(poziom<6)
+    {
+        linia(x,y,x+w/po(3,poziom-1),y,window);
+        linia(x,y+w/po(3,poziom-1),x+w/po(3,poziom-1),y+w/po(3,poziom-1),window);
+        linia(x+w/po(3,poziom-1),y,x+w/po(3,poziom-1),y+w/po(3,poziom-1),window);
+        linia(x,y,x,y+w/po(3,poziom-1),window);
+        kwadraty(x,y,poziom+1,window);
+        kwadraty(x+w/po(3,poziom),y,poziom+1,window);
+        kwadraty(x+2*w/po(3,poziom),y,poziom+1,window);
+        kwadraty(x+2*w/po(3,poziom),y+w/po(3,poziom),poziom+1,window);
+        kwadraty(x,y+w/po(3,poziom),poziom+1,window);
+        kwadraty(x+w/po(3,poziom),y+2*w/po(3,poziom),poziom+1,window);
+        kwadraty(x+2*w/po(3,poziom),y+2*w/po(3,poziom),poziom+1,window);
+        kwadraty(x,y+2*w/po(3,poziom),poziom+1,window);
     }
 }
-void kwadraty(int x,int y,int w,sf::RenderWindow &window)
+void trojkaty(int x,int y,int poziom,sf::RenderWindow &window)
 {
-    if(w>2)
+    if(poziom<6)
     {
-        linia(x,y,x+w,y,window);
-        linia(x,y+w,x+w,y+w,window);
-        linia(x+w,y,x+w,y+w,window);
-        linia(x,y,x,y+w,window);
-        kwadraty(x,y,w/3,window);
-        kwadraty(x+w/3,y,w/3,window);
-        kwadraty(x+w*2/3,y,w/3,window);
-        kwadraty(x+w*2/3,y+w/3,w/3,window);
-        kwadraty(x,y+w/3,w/3,window);
-        kwadraty(x+w/3,y+w*2/3,w/3,window);
-        kwadraty(x+w*2/3,y+w*2/3,w/3,window);
-        kwadraty(x,y+w*2/3,w/3,window);
+        linia(x,y+w/po(2,poziom-1),x+w/po(2,poziom-1),y+w/po(2,poziom-1),window);
+        linia(x+w/po(2,poziom),y,x+w/po(2,poziom-1),y+w/po(2,poziom-1),window);
+        linia(x+w/po(2,poziom),y,x,y+w/po(2,poziom-1),window);
+        trojkaty(x+w/po(2,poziom+1),y,poziom+1,window);
+        trojkaty(x,y+w/po(2,poziom),poziom+1,window);
+        trojkaty(x+w/po(2,poziom),y+w/po(2,poziom),poziom+1,window);
     }
 }
-void drzewo(int x,int y,int w,int h,sf::RenderWindow &window)
+void drzewo(int x,int y,int poziom,int kierunek,sf::RenderWindow &window)
 {
-    if(h>20)
+    if(kierunek<-3)
+        kierunek=4;
+    if(kierunek>4)
+        kierunek=-3;
+    if(poziom<13)
     {
-        linia(x,y,x-w,y-h,window);
-        linia(x,y,x+w,y-h,window);
-        drzewo(x-w,y-h,w/2,h/2,window);
-        drzewo(x+w,y-h,w/2,h/2,window);
+        int xtmp=x;
+        int ytmp=y;
+        if(kierunek==-1 || kierunek==-3)
+            xtmp+=x/sqrt(2)/po(2,poziom);
+        if(kierunek==-2)
+            xtmp+=x/po(2,poziom);
+        if(kierunek==1 || kierunek==3)
+            xtmp-=x/sqrt(2)/po(2,poziom);
+        if(kierunek==2)
+            xtmp-=x/po(2,poziom);
+        if(kierunek==-3 || kierunek==3)
+            ytmp+=y/sqrt(2)/po(2,poziom);
+        if(kierunek==4)
+            ytmp+=y/po(2,poziom);
+        if(kierunek==-1 || kierunek==1)
+            ytmp-=y/sqrt(2)/po(2,poziom);
+        if(kierunek==0)
+            ytmp-=y/po(2,poziom);
+        linia(x,y,xtmp,ytmp,window);
+        drzewo(xtmp,ytmp,poziom+1,kierunek+1,window);
+        drzewo(xtmp,ytmp,poziom+1,kierunek-1,window);
+            /*drzewo(x1,y1,(x1+x2)/3,(y1+y2)/3,window);
+            drzewo(x1,y1,(x1+x2)/3,(y1+y2)/3,window);*/
     }
-    else
+}
+void kantor(int x1,int x2,int poziom,sf::RenderWindow &window)
+{
+    if(poziom<10)
     {
-        sf::CircleShape shape(10.f);
-        shape.setFillColor(sf::Color::Green);
-        shape.setPosition(x-10,y-10);
-        window.draw(shape);
+        linia(x1,poziom*40,x2,poziom*40,window);
+        kantor(x1,x1+(x2-x1)/3,poziom+1,window);
+        kantor(x1+(x2-x1)*2/3,x2,poziom+1,window);
     }
 }
 int main()
 {
-    srand(time(NULL));
-    sf::RenderWindow window(sf::VideoMode(wx,wy), "SFML works!");
-    window.display();
-    while (window.isOpen())
+    printf("%d",po(3,0));
+    int odp;
+    sf::RenderWindow window(sf::VideoMode(w,w), "Fraktale");
+    do
     {
-        sf::Event event;
-        while (window.pollEvent(event))
+        printf("Menu :\n\
+               1. Dywan Sierpińskiego\n\
+               2. Trójkąt Sierpińskiego\n\
+               3. Kocha\n\
+               4. Drzewo binarne\n\
+               5. Zbiór Cantora\n\
+               6. Zbiór Julii\n");
+        scanf("%d",&odp);
+        system("cls");
+        switch(odp)
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+        case 1:
+            while (window.isOpen())
+            {
+                sf::Event event;
+                while (window.pollEvent(event))
+                {
+                    if (event.type == sf::Event::Closed)
+                        window.close();
+                    }
+                window.clear();
+                kwadraty(0,0,1,window);
+                window.display();
+            }
+            break;
+        case 2:
+            while (window.isOpen())
+            {
+                sf::Event event;
+                while (window.pollEvent(event))
+                {
+                    if (event.type == sf::Event::Closed)
+                        window.close();
+                    }
+                window.clear();
+                trojkaty(0,0,1,window);
+                window.display();
+                window.setSize(sf::Vector2u(w,w*sqrt(3)/2));
+            }
+        break;
+        case 4:
+            while (window.isOpen())
+            {
+                sf::Event event;
+                while (window.pollEvent(event))
+                {
+                    if (event.type == sf::Event::Closed)
+                        window.close();
+                    }
+                window.clear();
+                drzewo(w/2,w,1,0,window);
+                window.display();
+            }
+        break;
+        case 5:
+            while (window.isOpen())
+            {
+                sf::Event event;
+                while (window.pollEvent(event))
+                {
+                    if (event.type == sf::Event::Closed)
+                        window.close();
+                    }
+                window.clear();
+                kantor(0,w,1,window);
+                window.display();
+            }
+        break;
+        case 7:
+            window.close();
+            printf("KONIEC");
+        break;
+        default :
+            printf("zla odpowiedz");
+        break;
         }
-        window.clear();
-        
-        //do wyboru
-        
-        //kantor(0,wx,1,window);
-        //kwadraty(0,0,min(wx,wy),window);
-        //drzewo(wx/2,wy-1,wx/4,d_height*2,window);
-        window.display();
+        printf("\n\n");
     }
+    while(odp!=7);
     return 0;
 }
