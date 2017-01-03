0297ce65204d77b1b0cb1ca411973caf22071c06
Marcel <marcel2012@users.noreply.github.com>
2017-01-03 20:15:25 +0100

    Update fraktale.cpp

--- fraktale/fraktale.cpp
+++ fraktale/fraktale.cpp
@@ -1,31 +1,31 @@
 #include <SFML/Graphics.hpp>
 #include <cmath>
-int w=1000;
-int dokladnosc=8;
+//#include <windows.h>
+int w=800;//rozmiar okna
+int dokladnosc=1;//dok³adnoœæ liczenia zbioru julii
+int rekurencja_poziom=6;
+unsigned int real_time=0;
 struct zespolona
 {
+    //ustaw parametry zbioru Julii
     float r=-0.1,i=0.65;
-};/*
-struct zespolona
-{
-    float r=-0.390541,i=0.586788;
-};*/
+};
 zespolona c;
-inline zespolona kwadrat(zespolona a)
+inline zespolona kwadrat(zespolona a)//potêgowanie liczb zespolonych
 {
     zespolona tmp;
     tmp.r=a.r*a.r-a.i*a.i;
     tmp.i=2*a.r*a.i;
     return tmp;
 }
-inline zespolona dodawanie(zespolona a,zespolona b)
+inline zespolona dodawanie(zespolona a,zespolona b)//dodawanie liczb zespolonych
 {
     zespolona tmp;
     tmp.r=a.r+b.r;
     tmp.i=a.i+b.i;
     return tmp;
 }
-inline int poziom(zespolona d,zespolona c)
+inline int poziom(zespolona d,zespolona c)//sprawdzanie czy punkt po przekszta³ceniach d¹¿y do nieskoñczonoœci
 {
     for(int i=0; i<dokladnosc*255; i++)
     {
@@ -35,22 +35,25 @@ inline int poziom(zespolona d,zespolona c)
     }
     return 255;
 }
-inline void linia(int x1,int y1,int x2,int y2,sf::RenderWindow &window)
+inline void linia(int x1,int y1,int x2,int y2,sf::RenderWindow &window)//rysowanie linii
 {
     sf::Vertex point[2];
     point[0].position=sf::Vector2f(x1,y1);
     point[1].position=sf::Vector2f(x2,y2);
     window.draw(point,2, sf::Lines);
+    if(real_time)
+        window.display();
 }
-inline void punkt(int x1,int y1,sf::RenderWindow &window,int color)
+inline void punkt(int x1,int y1,sf::RenderWindow &window,int color)//kolorowanie piksela
 {
-    //color=255-color;
     sf::Color col(color,color,color);
     sf::Vertex point[1];
     point[0]=sf::Vertex(sf::Vector2f(x1,y1),col);
     window.draw(point,1, sf::Points);
+    if(real_time)
+        window.display();
 }
-inline int po(int a,int b)
+inline int po(int a,int b)//potêgowanie takie, ¿e n^0==1
 {
     int odp=1;
     while(b>=1)
@@ -62,7 +65,7 @@ inline int po(int a,int b)
 }
 inline void kwadraty(int x,int y,int poziom,sf::RenderWindow &window)
 {
-    if(poziom<6)
+    if(poziom<rekurencja_poziom)
     {
         linia(x,y,x+w/po(3,poziom-1),y,window);
         linia(x,y+w/po(3,poziom-1),x+w/po(3,poziom-1),y+w/po(3,poziom-1),window);
@@ -80,7 +83,7 @@ inline void kwadraty(int x,int y,int poziom,sf::RenderWindow &window)
 }
 inline void trojkaty(int x,int y,int poziom,sf::RenderWindow &window)
 {
-    if(poziom<6)
+    if(poziom<rekurencja_poziom)
     {
         linia(x,y+w/po(2,poziom-1),x+w/po(2,poziom-1),y+w/po(2,poziom-1),window);
         linia(x+w/po(2,poziom),y,x+w/po(2,poziom-1),y+w/po(2,poziom-1),window);
@@ -92,11 +95,20 @@ inline void trojkaty(int x,int y,int poziom,sf::RenderWindow &window)
 }
 inline void drzewo(int x,int y,int poziom,int kierunek,sf::RenderWindow &window)
 {
+    //kierunwk :
+    //0 góra
+    //-1 prawo góra
+    //-2 prawo
+    //-3 prawo dół
+    //1 lewo góra
+    //2 lewo
+    //3 lewo dół
+    //4 dol
     if(kierunek<-3)
         kierunek=4;
     if(kierunek>4)
         kierunek=-3;
-    if(poziom<13)
+    if(poziom<rekurencja_poziom)
     {
         int xtmp=x;
         int ytmp=y;
@@ -121,35 +133,77 @@ inline void drzewo(int x,int y,int poziom,int kierunek,sf::RenderWindow &window)
         drzewo(xtmp,ytmp,poziom+1,kierunek-1,window);
     }
 }
+inline void koch(int x1,int y1,int x2,int y2,int poziom,sf::RenderWindow &window)
+{
+    int x4=x1-(x1-x2)/3,y4=y1-(y1-y2)/3,x5=x2-(x2-x1)/3,y5=y2-(y2-y1)/3;
+    int x3=(x5-x4)*cos(M_PI/3)-(y5-y4)*sin(M_PI/3)+x4,y3=(x5-x4)*sin(M_PI/3)+(y5-y4)*cos(M_PI/3)+y4;
+    if(poziom<rekurencja_poziom)
+    {
+        koch(x1,y1,x4,y4,poziom+1,window);
+        koch(x5,y5,x2,y2,poziom+1,window);
+        koch(x4,y4,x3,y3,poziom+1,window);
+        koch(x3,y3,x5,y5,poziom+1,window);
+    }
+    else
+    {
+        linia(x4,y4,x3,y3,window);
+        linia(x3,y3,x5,y5,window);
+        linia(x1,y1,x2,y2,window);
+    }
+}
 inline void kantor(int x1,int x2,int poziom,sf::RenderWindow &window)
 {
-    if(poziom<10)
+    if(poziom<rekurencja_poziom)
     {
         linia(x1,poziom*40,x2,poziom*40,window);
         kantor(x1,x1+(x2-x1)/3,poziom+1,window);
         kantor(x1+(x2-x1)*2/3,x2,poziom+1,window);
     }
 }
+inline void zbior_julii(std::vector <std::vector<int> > &pole,sf::RenderWindow &window)
+{
+    for(int i=0; i<w; i++)
+        for(int j=0; j<w; j++)
+            if(pole.at(i).at(j))
+                punkt(i,w-j,window,pole.at(i).at(j));
+}
 int main()
 {
-    printf("%d",po(3,0));
     int odp;
-    do
+    printf("\n\n");
+    while(true)
     {
         printf("Menu :\n\
-               1. Dywan Sierpińskiego\n\
-               2. Trójkąt Sierpińskiego\n\
-               3. (X) Płatek Kocha\n\
-               4. Drzewo binarne\n\
-               5. Zbiór Cantora\n\
-               6. Zbiór Julii\n\
-               7. Koniec\n");
+                1. Dywan Sierpinskiego\n\
+                2. Trojkat Sierpinskiego\n\
+                3. (X) Platek Kocha\n\
+                4. Drzewo binarne\n\
+                5. Zbior Cantora\n\
+                6. Zbior Julii\n\
+                \n\
+                Ustawienia dodatkowe\n\
+                \n\
+                Maksymalny poziom rekurencji : %d\n\
+                Parametr c Zbioru Julii : %f %f\n\
+                Dokladnosc liczenia Zbioru Julii: %d\n\
+                Tryb rysowania : %s\n\
+                \n\
+                7. Edytuj maksymalny poziom rekurencji\n\
+                8. Edytuj parametr c Zbioru Julii\n\
+                9. Edytuj dokladnosc liczenia Zbioru Julii\n\
+                10. Edytuj rodzaj rysowania\n\
+                \n\
+                11. Koniec\n\
+                \n\
+                Podaj liczbe : \
+               ",rekurencja_poziom,c.r,c.i,dokladnosc,(real_time==0?"raz a dobrze":"w czasie rzeczywistym"));
         scanf("%d",&odp);
+        system("cls");
         switch(odp)
         {
         case 1:
         {
-            sf::RenderWindow window(sf::VideoMode(w,w), "Fraktale");
+            sf::RenderWindow window(sf::VideoMode(w,w), "Dywan sierpinskiego");
             while (window.isOpen())
             {
                 sf::Event event;
@@ -166,7 +220,7 @@ int main()
         break;
         case 2:
         {
-            sf::RenderWindow window(sf::VideoMode(w,w), "Fraktale");
+            sf::RenderWindow window(sf::VideoMode(w,w), "Trojkat Sierpinskiego");
             while (window.isOpen())
             {
                 sf::Event event;
@@ -182,9 +236,30 @@ int main()
             }
         }
         break;
+        case 3:
+        {
+            sf::RenderWindow window(sf::VideoMode(w,w), "Koch");
+            while (window.isOpen())
+            {
+                sf::Event event;
+                while (window.pollEvent(event))
+                {
+                    if (event.type == sf::Event::Closed)
+                        window.close();
+                }
+                window.clear();
+                int x5=100,y5=w-200,x4=w-100,y4=w-200;
+                int x3=(x5-x4)*cos(M_PI/3)-(y5-y4)*sin(M_PI/3)+x4,y3=(x5-x4)*sin(M_PI/3)+(y5-y4)*cos(M_PI/3)+y4;
+                koch(x5,y5,x4,y4,0,window);
+                koch(x4,y4,x3,y3,0,window);
+                koch(x3,y3,x5,y5,0,window);
+                window.display();
+            }
+        }
+        break;
         case 4:
         {
-            sf::RenderWindow window(sf::VideoMode(w,w), "Fraktale");
+            sf::RenderWindow window(sf::VideoMode(w,w), "Drzewo binarne");
             while (window.isOpen())
             {
                 sf::Event event;
@@ -201,7 +276,7 @@ int main()
         }
         case 5:
         {
-            sf::RenderWindow window(sf::VideoMode(w,w), "Fraktale");
+            sf::RenderWindow window(sf::VideoMode(w,w), "Cantor");
             while (window.isOpen())
             {
                 sf::Event event;
@@ -218,7 +293,7 @@ int main()
         break;
         case 6:
         {
-            sf::RenderWindow window(sf::VideoMode(w,w), "Fraktale");
+            sf::RenderWindow window(sf::VideoMode(w,w), "Zbior Julii");
             std::vector <int> linia(w,0);
             std::vector <std::vector<int> > pole(w,linia);
             int skala=3;
@@ -238,15 +313,39 @@ int main()
                         window.close();
                 }
                 window.clear();
-                for(int i=0; i<w; i++)
-                    for(int j=0; j<w; j++)
-                        punkt(i,w-j,window,pole.at(i).at(j));
+                zbior_julii(pole,window);
                 window.display();
             }
         }
         break;
         case 7:
+            system("cls");
+            printf("Podaj maksymalny poziom rekurencji : ");
+            scanf("%d",&rekurencja_poziom);
+            break;
+        case 8:
+            system("cls");
+            printf("Podaj parametry c Zbioru Julii : ");
+            scanf("%f%f",&c.r,&c.i);
+            break;
+        case 9:
+            system("cls");
+            printf("Podaj dokladnosc liczenia Zbioru Julii : ");
+            scanf("%d",&dokladnosc);
+            break;
+        case 10:
+            system("cls");
+            printf("0 - raz a dobrze (szybkie)\n1 - w czasie rzeczywistym (wolne): ");
+            scanf("%d",&real_time);
+            if(real_time>1)
+            {
+                printf("zla odpowiedz\nustawiono wartosc domyslna");
+                real_time=0;
+            }
+            break;
+        case 11:
             printf("KONIEC");
+            return 0;
             break;
         default :
             printf("zla odpowiedz");
@@ -254,6 +353,5 @@ int main()
         }
         printf("\n\n");
     }
-    while(odp!=7);
     return 0;
 }
