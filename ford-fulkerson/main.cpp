a8dae045c16b6fc6c3bd7f734a1c26bb2c705225
Marcel <marcel2012@users.noreply.github.com>
2017-05-24 17:56:21 +0200

    xd5

--- ford-fulkerson/main.cpp
+++ ford-fulkerson/main.cpp
@@ -2,21 +2,25 @@
 #include <vector>
 int n,m;
 int s,t;
-int dfs(std::vector<std::vector<int> > &dane,std::vector<bool> &odwiedzony,std::vector<std::vector<int> > &przeplyw,int start,int rodzic,int minflow)
+int wynik=0;
+int dfs(std::vector<std::vector<int> > &sasiedzi,std::vector<bool> &odwiedzony,std::vector<std::vector<int> > &przeplyw,int start,int rodzic,int minflow)
 {
     if(start==t)
+    {
+        wynik+=minflow;
         return minflow;
+    }
     odwiedzony[start]=true;
     int maxflow=0;
-    for(int i=0;i<dane[start].size();i++)
-        if(przeplyw[start][dane[start][i]]>0)
-            if(!odwiedzony[dane[start][i]])
+    for(int i=0;i<sasiedzi[start].size();i++)
+        if(przeplyw[start][sasiedzi[start][i]]>0)
+            if(!odwiedzony[sasiedzi[start][i]])
             {
-                int uzyte=std::min(minflow,dfs(dane,odwiedzony,przeplyw,dane[start][i],start,std::min(minflow,przeplyw[start][dane[start][i]])));
+                int uzyte=dfs(sasiedzi,odwiedzony,przeplyw,sasiedzi[start][i],start,std::min(minflow,przeplyw[start][sasiedzi[start][i]]));
                 if(uzyte)
                 {
-                    przeplyw[start][dane[start][i]]-=uzyte;
-                    przeplyw[dane[start][i]][start]+=uzyte;
+                    przeplyw[start][sasiedzi[start][i]]-=uzyte;
+                    przeplyw[sasiedzi[start][i]][start]+=uzyte;
                     minflow-=uzyte;
                     maxflow+=uzyte;
                 }
@@ -27,7 +31,7 @@ int dfs(std::vector<std::vector<int> > &dane,std::vector<bool> &odwiedzony,std::
 int main()
 {
     scanf("%d%d",&n,&m);
-    std::vector<std::vector<int> > dane(n);
+    std::vector<std::vector<int> > sasiedzi(n);
     std::vector<bool> odwiedzony(n);
     std::vector<int> tmp(n);
     std::vector<std::vector<int> > przeplyw(n,tmp);
@@ -35,15 +39,12 @@ int main()
     {
         int a,b,c;
         scanf("%d%d%d",&a,&b,&c);
-        dane[a].push_back(b);
-        dane[b].push_back(a);
+        sasiedzi[a].push_back(b);
+        sasiedzi[b].push_back(a);
         przeplyw[a][b]=c;
     }
     scanf("%d%d",&s,&t);
-    while(dfs(dane,odwiedzony,przeplyw,s,-1,100000000));
-    int wynik=0;
-    for(int i=0;i<dane[t].size();i++)
-        wynik+=przeplyw[t][dane[t][i]];
+    while(dfs(sasiedzi,odwiedzony,przeplyw,s,-1,100000000));
     printf("wynik: %d\n",wynik);
     return 0;
 }
