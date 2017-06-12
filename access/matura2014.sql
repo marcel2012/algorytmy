4aa5414b717a2ab0f82e1296af248dfd68d32337
Marcel <marcel2012@users.noreply.github.com>
2017-06-12 18:23:33 +0200

    Create matura2014.sql

---
+++ access/matura2014.sql
@@ -0,0 +1,15 @@
+a)
+
+SELECT "Wiek", SUM( 1 ) FROM "dzieci" GROUP BY "Wiek"
+
+b)
+
+SELECT SUM( 1 ), "Plec", "Id_przedszkola" FROM "dzieci" WHERE "Plec" = 'chlopiec' AND "Id_przedszkola" = ( SELECT "Id_przedszkola" FROM "przedszkola" WHERE "Nazwa_przedszkola" = 'Przedszkole nr 28 Srebrna Kotwica' ) OR "Id_przedszkola" = ( SELECT "Id_przedszkola" FROM "przedszkola" WHERE "Nazwa_przedszkola" = 'Przedszkole nr 49 im. Panienki z Okienka' ) OR "Id_przedszkola" = ( SELECT "Id_przedszkola" FROM "przedszkola" WHERE "Nazwa_przedszkola" = 'Przedszkole nr 87 Gwiezdna Kraina' ) GROUP BY "Plec", "Id_przedszkola"
+
+c)
+
+SELECT "Id_przedszkola", COUNT( * ) FROM "dzieci" WHERE "Wiek" = 3 GROUP BY "Id_przedszkola" ORDER BY COUNT( * ) DESC
+
+d)
+
+SELECT "Id_przedszkola", COUNT( * ) - ( SELECT "Liczba_miejsc" FROM "przedszkola" WHERE "Id_przedszkola" = "dzieci"."Id_przedszkola" ) FROM "dzieci" GROUP BY "Id_przedszkola" ORDER BY COUNT( * ) DESC
