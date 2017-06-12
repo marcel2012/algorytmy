562c84d9d62981a9013ef043fb5fc3de1e809391
Marcel <marcel2012@users.noreply.github.com>
2017-06-12 18:37:40 +0200

    Update matura2014.sql

--- access/matura2014.sql
+++ access/matura2014.sql
@@ -4,7 +4,7 @@ SELECT "Wiek", SUM( 1 ) FROM "dzieci" GROUP BY "Wiek"
 
 b)
 
-SELECT SUM( 1 ), "Plec", "Id_przedszkola" FROM "dzieci" WHERE "Plec" = 'chlopiec' AND "Id_przedszkola" = ( SELECT "Id_przedszkola" FROM "przedszkola" WHERE "Nazwa_przedszkola" = 'Przedszkole nr 28 Srebrna Kotwica' ) OR "Id_przedszkola" = ( SELECT "Id_przedszkola" FROM "przedszkola" WHERE "Nazwa_przedszkola" = 'Przedszkole nr 49 im. Panienki z Okienka' ) OR "Id_przedszkola" = ( SELECT "Id_przedszkola" FROM "przedszkola" WHERE "Nazwa_przedszkola" = 'Przedszkole nr 87 Gwiezdna Kraina' ) GROUP BY "Plec", "Id_przedszkola"
+SELECT SUM( 1 ), "Plec", "Id_przedszkola" FROM "dzieci" WHERE "Id_przedszkola" = ( SELECT "Id_przedszkola" FROM "przedszkola" WHERE "Nazwa_przedszkola" = 'Przedszkole nr 28 Srebrna Kotwica' ) OR "Id_przedszkola" = ( SELECT "Id_przedszkola" FROM "przedszkola" WHERE "Nazwa_przedszkola" = 'Przedszkole nr 49 im. Panienki z Okienka' ) OR "Id_przedszkola" = ( SELECT "Id_przedszkola" FROM "przedszkola" WHERE "Nazwa_przedszkola" = 'Przedszkole nr 87 Gwiezdna Kraina' ) GROUP BY "Plec", "Id_przedszkola"
 
 c)
 
